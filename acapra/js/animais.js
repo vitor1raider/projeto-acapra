9e2abc833478054a5a4721cfa7fd0d81e6e1c290
vitor1raider <124637474+vitor1raider@users.noreply.github.com>
2025-08-25 20:11:44 -0300

    dados dos animais

---
+++ acapra/js/animais.js
@@ -0,0 +1,13 @@
+const animais = [
+    {
+        id: 2,
+        nome: "Barto",
+        descricao: "Barto é um gatinho macho, que foi encontrado em uma fábrica de peças automotivas (por isso o nome), chegou em estado caquético, com extrema magreza. Aos poucos foi se recuperando e hoje é um jovem 100% saudável e cheio de amor!",
+        observacao: "Se dá bem com outros animais (cães e gatos).",
+        imagem: "img/barto.PNG",
+        alt: "Barto - Gato",
+        tituloModal: "Barto | macho, 3 meses, castrado e vacinado"
+  }
+];
+
+export default animais;
