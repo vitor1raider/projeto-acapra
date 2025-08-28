cf2b3cc92c7f7bca6db7ff420553b1f2237db42f
vitor1raider <vitorhdt.r@gmail.com>
2025-08-27 21:19:18 -0300

    endpoint api usuarios/animais atualizados

--- acapra/js/server.js
+++ acapra/js/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import animais from './animais.js'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -30,11 +29,7 @@ app.get('/cadastrar', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'cadastrarAnimal.html'))
 })
 
-// Endpoint API - Dados dos animais cadastrados
-app.get('/api/dadosAnimais', (req, res) => {
-    res.json(animais);
-})
-
+// Endpoint API - Dados dos usuárioss
 app.post('/usuarios', async (req, res) => {
     await prisma.Usuario.create({
         data: {
@@ -48,21 +43,22 @@ app.post('/usuarios', async (req, res) => {
     res.status(201).json(req.body)
 })
 
+// Endpoint API - Dados dos animais cadastrados
 app.post('/animais', async (req, res) => {
     await prisma.Animais.create({
         data: {
             nome: req.body.nome,
-        especie: req.body.especie,
-        sexo: req.body.sexo,
-        castracao: req.body.castracao,
-        vacina: req.body.vacina,
-        idade: req.body.idade,
-        porte: req.body.porte,
-        status: req.body.status,
-        data_resgate: req.body.data_resgate,
-        observacoes: req.body.observacoes,
-        obersavacoes_medicas: req.body.obersavacoes_medicas,
-        raca: req.body.raca,
+            especie: req.body.especie,
+            sexo: req.body.sexo,
+            castracao: req.body.castracao,
+            vacina: req.body.vacina,
+            idade: req.body.idade,
+            porte: req.body.porte,
+            status: req.body.status,
+            data_resgate: req.body.data_resgate,
+            observacoes: req.body.observacoes,
+            obersavacoes_medicas: req.body.obersavacoes_medicas,
+            raca: req.body.raca,
         }
     })
 
@@ -70,8 +66,13 @@ app.post('/animais', async (req, res) => {
 })
 
 app.get('/usuarios', async (req, res) => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.usuario.findMany()
     res.status(201).json(users)
+})
+
+app.get('/animais', async (req, res) => {
+    const animais = await prisma.animais.findMany()
+    res.status(201).json(animais)
 })
 
 app.listen(port)
